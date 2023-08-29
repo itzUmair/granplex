4d17f7c9b5e42abcca822c89f593902e1204266d
umair <muhammadumair2511@gmail.com>
2023-08-29 13:15:56 +0500

    fix: fixed authentication middleware

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,3 +1,8 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const logger = async (req, res, next) => {
   const requestTime = new Date().toLocaleString();
   console.log(`${req.method} => ${req.url} @ ${requestTime}`);
@@ -6,6 +11,7 @@ export const logger = async (req, res, next) => {
 
 export const authentication = (req, res, next) => {
   const authCookie = req.cookies._auth;
+  console.log(authCookie);
 
   if (req.url.endsWith("/signin") || req.url.endsWith("/signup")) {
     next();
@@ -16,5 +22,12 @@ export const authentication = (req, res, next) => {
     return res
       .status(401)
       .send({ message: "You are not authorized to access the API." });
+  }
+
+  try {
+    jwt.verify(authCookie, process.env.JWT_SECRET);
+    next();
+  } catch (error) {
+    return res.status(401).send({ message: "Invalid token." });
   }
 };
